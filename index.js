a7b36a821b8e44dab39b1cf40e9f0d0b040beddc
DanBehrman <dan.behrman@wework.com>
2019-09-14 17:22:54 -0400

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someNumber){
+  if (someNumber < 200) {
+    return "This one is on me!";
+  }
+  else if (someNumber > 2000 && someNumber < 2500) {
+    return "I will gladly take your thirty bucks.";
+  }
+  else {
+    return "No can do.";
+  }
+
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return city === "NYC" ? "Ok, sounds good." : "No go."
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch(tip) {
+    case 'generous':
+      return 'Thank you so much.';
+      break;
+    case 'not as generous':
+      return 'Thank you.'
+      break;
+    default:
+      return 'Bye.';
+  }
 }
