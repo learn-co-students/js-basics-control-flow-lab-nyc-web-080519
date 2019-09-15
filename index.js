83119362dd5b5eb3cf81ef1cae4ac26b7f43b525
Andy Choi <wooseokchoi96@gmail.com>
2019-09-15 09:03:48 -0400

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(rideDistance){
   // Write your code here!
+  let response;
+  if (rideDistance <= 400) {
+    response = 'This one is on me!';
+  } else if (rideDistance > 2000 && rideDistance <= 2500) {
+    response = 'I will gladly take your thirty bucks.';
+  } else {
+    response = 'No can do.';
+  }
+  return response;
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
 }
